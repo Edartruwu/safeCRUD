87e36342c4c4d51ac66d9676fac4562e186db709
Edartruwu <edartru.contactos@gmail.com>
2024-11-01 00:12:34 -0500

    add get method

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import { ContentType, POST, HttpError, ValidationError } from "./methods/post";
+import { POST } from "./methods/post";
 
 import type { PostConfig } from "./methods/post";
 
-export type { PostConfig };
+export type { PostConfig, GetConfig };
 
-export { ContentType, POST, HttpError, ValidationError };
+import { ContentType } from "./types/enums";
+import { HttpError, ValidationError } from "./utils/classes";
+
+import { GET } from "./methods/get";
+import type { GetConfig } from "./methods/get";
+export { ContentType, POST, HttpError, ValidationError, GET };
